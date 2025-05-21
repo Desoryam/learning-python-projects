ed11a0684170f53d2066038b62b095e18fa424b7
Desoryam <anasgamerlol8@gmail.com>
2025-05-21 14:34:21 +0530

    final code

--- practice/prac2.py
+++ practice/prac2.py
@@ -1,5 +1,36 @@
 import random as r
 words=["Octopus"]
 
-r_w=(r.choice(words)).upper()
-print(r_w)
+r_w=list((r.choice(words)).upper())
+g=7
+print("WELCOME TO HANGMAN".center(150))
+print("YOUR WORD IS :")
+g_w=[]
+for i in r_w:
+    g_w.append("_")
+
+print(g_w)
+print()
+while True:
+    print(f"GUESSES LEFT : {g}")
+    p=input("Pick a letter:").upper()
+    if p in "".join(r_w):
+        for n,w in enumerate(r_w):
+            if p==w:
+                g_w[n]=p
+            elif p!=w and  g_w[n]!="_":
+                pass
+            else:
+                pass
+    else:
+        g=g-1
+    print(g_w)
+    if g==0:
+        print("YOU LOST")
+        print("\nTHANK YOU FOR PLAYING :)")
+        break
+    fw="".join(g_w)
+    if "_" not in fw:
+        print("YOU WON")
+        print("\nTHANK YOU FOR PLAYING :)")
+        break
