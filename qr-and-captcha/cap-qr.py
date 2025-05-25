97081498fa365ac195a48f9681404d961ef93854
Desoryam <anasgamerlol8@gmail.com>
2025-05-25 23:38:26 +0530

    new

---
+++ qr-and-captcha/cap-qr.py
@@ -0,0 +1,16 @@
+import qrcode
+import base64
+
+with open("image/img1.png", "rb") as image_file:
+    encoded_string = base64.b64encode(image_file.read())
+
+qr = qrcode.QRCode(version=2, box_size=10, border=5)
+
+qr.add_data(encoded_string)
+
+qr.make()
+
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("output/MYQR1.png")
+print("QR code generated successfully!")
